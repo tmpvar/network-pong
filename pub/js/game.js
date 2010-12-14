244c8c3077cc35bc868649d8d46e963fa9e56d8c
Elijah Insua <tmpvar@gmail.com>
2010-12-13 22:44:08 -0500

    locking the local paddle into the bounds of the screen

--- pub/js/game.js
+++ pub/js/game.js
@@ -16,15 +16,16 @@
   ns.game    = carena.build({
   }, ['carena.Renderable', 'carena.Eventable', 'carena.Box', 'carena.Eventable']);
   // Trick the collision detection
-  ns.game.localBounds   = function() { return { x:0,y:0, x2: 900, y2: 900} };
-  ns.game.containsPoint = function() { return true; };
+  ns.game.localBounds   = function() {
+    return { x:0,y:0, x2: ns.canvas.width, y2: ns.canvas.height}
+  };
 
   ns.paddles = {
     local  : paddleRenderStep(carena.build({
       width: 100,
-      height: 30,
+      height: 15,
       x : 0,
-      y : ns.canvas.height-30,
+      y : ns.canvas.height-15,
       style : {
         backgroundColor : "white"
       },
@@ -32,7 +33,7 @@
 
     remote : paddleRenderStep(carena.build({
       width: 100,
-      height: 30,
+      height: 15,
       x : 0,
       y : 0,
       style : {
@@ -64,10 +65,13 @@
 
   // Setup the events
   ns.game.event.bind("mouse.move", function(name, data) {
-    var x = data.mouse.x;
+    var half = ns.paddles.local.width/2,
+        x    = data.mouse.x-half;
 
-    if (x+ns.paddles.local.width > ns.canvas.width) {
-      x = ns.canvas.width - ns.paddles.local.width;
+    if (x+ns.paddles.local.width+half > ns.canvas.width) {
+      x = (ns.canvas.width - ns.paddles.local.width);
+    } else if (x < 0) {
+      x = 0;
     }
 
     ns.paddles.local.x = x;
